d031b5ff3bf749c37e4b9015d4d5c127d899382e
Alexander Baryshnikov <dev@baryshnikov.net>
2020-12-08 18:16:20 +0800

    ui: generate query tokens URL, remove JS

--- server/ui/utils.go
+++ server/ui/utils.go
@@ -1,6 +1,8 @@
 package ui
 
 import (
+	"net/http"
+	"net/url"
 	"strings"
 
 	"github.com/gin-gonic/gin"
@@ -10,6 +12,7 @@ type baseResponse struct {
 	Context    *gin.Context
 	Login      string
 	Authorized bool
+	BaseURL    string
 }
 
 func (br baseResponse) Rel(path string, tail ...string) string {
@@ -30,5 +33,21 @@ func base(gctx *gin.Context) baseResponse {
 		Authorized: gctx.GetBool(ctxAuthorized),
 		Context:    gctx,
 		Login:      gctx.GetString(ctxLogin),
+		BaseURL:    getProto(gctx.Request) + "://" + gctx.Request.Host,
 	}
+}
+
+func getProto(req *http.Request) string {
+	return extractScheme(req.Header.Get("Origin"), extractScheme(req.Header.Get("Referer"), "https"))
+}
+
+func extractScheme(guess, fallback string) string {
+	if guess == "" {
+		return fallback
+	}
+	u, err := url.Parse(guess)
+	if err != nil {
+		return fallback
+	}
+	return u.Scheme
 }
